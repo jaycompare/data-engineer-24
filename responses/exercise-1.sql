ee2336677394208ea6b8dda86c082f9519464070
jaycompare <166976321+jaycompare@users.noreply.github.com>
2024-04-14 01:43:45 -0500

    Update exercise-1.sql

--- responses/exercise-1.sql
+++ responses/exercise-1.sql
@@ -1,2 +1,8 @@
 -- write your SQL here
-select 1;
+SELECT  oi.locationId AS location_id , EXTRACT(YEAR_MONTH FROM t.datetime) AS month_year, SUM(refund.amount) AS total_refund_amount
+	FROM  transactions AS t, 
+		JSON_TABLE(t.details, '$.items[*]' COLUMNS(id INT PATH '$.id', amount INT PATH '$.amount')) refund
+	INNER JOIN orderItems oi ON oi.id=refund.id 
+	group by oi.locationId, month_year
+	order by MAX(t.datetime) DESC 
+	
